d1bd9d3001b3f3b6e768c30a7305ec688a70c72c
noryan2002 <noryan2002@gmail.com>
2025-11-19 17:05:10 +0200

    Update systemTest.java

--- stringCahin/src/test/java/stringCahin/stringCahin/systemTest.java
+++ stringCahin/src/test/java/stringCahin/stringCahin/systemTest.java
@@ -9,7 +9,7 @@ public class systemTest {
 	public void testCompleteChain() {
 		ChainMain runner = new ChainMain();
 		String result = runner.run();
-		assertEquals("HE", result);
+		assertEquals("HELLO", result);
 	}
 	
 	@Test
@@ -20,6 +20,17 @@ public class systemTest {
 		B b = new B(c);
 		A a = new A(b);
 		String result = a.process("");
-		assertEquals("HE", result);
+		assertEquals("HELLO", result);
 	}
+	Public void testSystem_negative(){
+		E e = new E();
+		D d = new D(e);
+		C c = new C(d);
+		B b = new B(c);
+		A a = new A(b);
+		String initialInput="123";
+		Stering result =a.process(initialInput);
+		assertEquals("123HELLO",result);}
+	
+		
 }
